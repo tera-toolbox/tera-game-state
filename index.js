0565ab69953ffc870f16903ff24f516de4500279
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-06-16 11:08:59 +0300

    update

--- index.js
+++ index.js
@@ -6,7 +6,54 @@ const GameStates = {
     INGAME: 2
 }
 
-class TeraGameState extends EventEmitter {
+class ClientMod {
+    constructor(mod) {
+        this.users = new Map;
+        this.items = new Map;
+        this.continents = new Map;
+        this.abnormalities = new Map;
+
+        mod.clientInterface.once('ready', async () => {
+            // UserData
+            (await mod.queryData('/UserData/Template/', [], true, false, ['id', 'class', 'race', 'gender'])).forEach(result => {
+                this.users.set(result.attributes.id, result.attributes);
+            });
+
+            // ContinentData
+            (await mod.queryData('/ContinentData/Continent/', [], true, false, ['id', 'channelType'])).forEach(result => {
+                this.continents.set(result.attributes.id, result.attributes);
+            });
+
+            // ItemData / StrSheet_Item
+            (await mod.queryData('/ItemData/Item/', [], true, false, ['id', 'combatItemType'])).forEach(result => {
+                this.items.set(result.attributes.id, result.attributes);
+            });
+            (await mod.queryData('/StrSheet_Item/String/', [], true, false)).forEach(result => {
+                let item = this.items.get(result.attributes.id);
+                if (item)
+                    Object.assign(item, {
+                        name: result.attributes.string,
+                        tooltip: result.attributes.toolTip
+                    });
+            });
+
+            // Abnormality / StrSheet_Abnormality
+            (await mod.queryData('/Abnormality/Abnormal/', [], true, true, ['id', 'bySkillCategory', 'infinity', 'time', 'method', 'tickInterval', 'type', 'value'])).forEach(result => {
+                this.abnormalities.set(result.attributes.id, Object.assign(result.attributes, { effects: result.children.map(effect => effect.attributes) }));
+            });
+            (await mod.queryData('/StrSheet_Abnormality/String/', [], true, false)).forEach(result => {
+                let abnormality = this.abnormalities.get(result.attributes.id);
+                if (abnormality)
+                    Object.assign(abnormality, {
+                        name: result.attributes.name,
+                        tooltip: result.attributes.tooltip
+                    });
+            });
+        });
+    }
+}
+
+class NetworkMod extends EventEmitter {
     constructor(mod) {
         super();
         this.setMaxListeners(0);
@@ -20,7 +67,7 @@ class TeraGameState extends EventEmitter {
         this.loadedSubmodules = {};
 
         // Make sure to load game data first
-        this.data = mod.require['tera-game-state-helper'];
+        this.data = mod.clientMod;
 
         // Now initialize default submodules
         this.installHooks();
@@ -117,4 +164,8 @@ class TeraGameState extends EventEmitter {
     get serverId() { return this.mod.serverId; }
 }
 
-module.exports = TeraGameState;
+module.exports = {
+    ClientMod,
+    NetworkMod,
+    RequireInterface: (globalMod, clientMod, networkMod, requiredBy) => networkMod,
+};
